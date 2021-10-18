4298056fcca251cf11e6c3d682ddb8cb02065d56
jacobhardman99 <61813749+jacobhardman99@users.noreply.github.com>
2021-10-18 17:11:40 -0400

    Fix

--- JumpyStreet/Assets/Scripts/PlayerMovement.cs
+++ JumpyStreet/Assets/Scripts/PlayerMovement.cs
@@ -13,12 +13,13 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /*
     public GameObject gameOverPanel;
     public GameObject pauseMenu;
 
     public Text scoreText;
     public Text highScoreText;
-
+    */
     private Rigidbody rb; //As 3D tradition, rigidbody is needed for movement
     private int score; //Every safe step is worth one point
     private int highScore; //High score will be the highest number of steps in one game
@@ -32,8 +33,10 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         score = 0;
         highScore = 0; //0 will be the default score for first time and whatever points players have scored, will replace the previous scores
+        /*
         gameOverPanel.SetActive(false);
         pauseMenu.SetActive(false);
+        */
         tm = GameObject.FindGameObjectWithTag("TerrainManager").GetComponent<TerrainManager>();
         if (tm == null)
         {
@@ -46,34 +49,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.position = new Vector3(transform.position.x + movementSpacer, transform.position.y, transform.position.z);
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x + movementSpacer), transform.position.y, Mathf.RoundToInt(transform.position.z));
             tm.playerPos = transform.position;
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.position = new Vector3(transform.position.x - movementSpacer, transform.position.y, transform.position.z);
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x - movementSpacer), transform.position.y, Mathf.RoundToInt(transform.position.z));
             tm.playerPos = transform.position;
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movementSpacer);
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), transform.position.y, Mathf.RoundToInt(transform.position.z + movementSpacer));
             tm.playerPos = transform.position;
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - movementSpacer);
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), transform.position.y, Mathf.RoundToInt(transform.position.z - movementSpacer));
             tm.playerPos = transform.position;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (transform.position.y <= 1.05f)
-            {
-                GetComponent<Rigidbody>().AddForce(Vector3.up * 150);
-            }
         }
     }
 
@@ -81,7 +76,8 @@ public class PlayerMovement : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Hazard"))
         {
-            gameOverPanel.SetActive(true);
+            Debug.LogWarning("Game Over");
+            //gameOverPanel.SetActive(true);
         }
 
         if (other.gameObject.CompareTag("FloorStep"))
@@ -89,7 +85,7 @@ public class PlayerMovement : MonoBehaviour
 
         }
     }
-
+    /*
     public void OnPauseButtonClick()
     {
         pauseMenu.SetActive(true);
@@ -112,4 +108,5 @@ public class PlayerMovement : MonoBehaviour
         gameOverPanel.SetActive(false);
         score = 0;
     }
+    */
 }
--- JumpyStreet/Assets/Scripts/PropMover.cs
+++ JumpyStreet/Assets/Scripts/PropMover.cs
@@ -15,6 +15,7 @@ public class PropMover : MonoBehaviour
             }
         }
     }
+    public bool playerCanStandOn = false;
 
     private Vector3 movementVector;
     private bool runMovement = false;
@@ -22,8 +23,11 @@ public class PropMover : MonoBehaviour
     private float RoadLeftEnd = 49f;
     private float roadRightEnd = -49f;
 
+    private GameObject terrainManager;
+
     private void Start()
     {
+        terrainManager = GameObject.FindWithTag("TerrainManager");
         transform.Rotate(new Vector3(0, ((movementVector.z / Mathf.Abs(movementVector.z)) * 90) - 90, 0));
     }
 
@@ -38,6 +42,22 @@ public class PropMover : MonoBehaviour
         else if (transform.position.z > RoadLeftEnd)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, roadRightEnd);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (playerCanStandOn && collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (playerCanStandOn && collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = terrainManager.transform;
         }
     }
 }
